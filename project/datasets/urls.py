b58ca6e014ce37269e2bffa553ff1e05ff5e6ffb
Anton Perekhozhuk <anton.perekhozhuk@gmail.com>
2021-01-20 02:28:25 +0200

    Create index page

--- project/datasets/urls.py
+++ project/datasets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
 
 urlpatterns = [
+    path('', views.index),
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view())
 ]
