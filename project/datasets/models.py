016b46d22d79568a6c39a3525b6277dbd780b2c1
Anton Perekhozhuk <anton.perekhozhuk@gmail.com>
2021-01-21 02:33:07 +0200

    Add models

--- project/datasets/models.py
+++ project/datasets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Schema(models.Model):
+
+    name = models.CharField(max_length = 50)
+    user = models.ForeignKey(User, related_name='schemas', on_delete=models.CASCADE)
+
+
+class SchemaColumn(models.Model):
+
+    name = models.CharField(max_length = 30)
+    datatype = models.CharField(max_length = 50)
+    order = models.IntegerField()
+    schema = models.ForeignKey(Schema, related_name='columns', on_delete=models.CASCADE)
